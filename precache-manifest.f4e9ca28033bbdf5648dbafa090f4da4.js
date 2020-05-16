664831ed3d5554cf7f837c5422376732c8b57ad2
Mathurah Ravigulan <mathurahravigulan@deloitte.ca>
2020-05-16 15:03:54 -0400

    Updates

---
+++ precache-manifest.f4e9ca28033bbdf5648dbafa090f4da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ea660df5671987872a6ce23cec2854",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "b20113d33a1d62d75395",
+    "url": "/pokemon/static/css/main.4fc1c245.chunk.css"
+  },
+  {
+    "revision": "b28f47b46317bc3ab482",
+    "url": "/pokemon/static/js/2.44cc375b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.44cc375b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20113d33a1d62d75395",
+    "url": "/pokemon/static/js/main.31d20ae6.chunk.js"
+  },
+  {
+    "revision": "a9746ebec628315a90d7",
+    "url": "/pokemon/static/js/runtime-main.7ddc8b87.js"
+  }
+]);
